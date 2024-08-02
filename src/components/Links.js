2af71fe70fea2c2399d454bbc33e8ec6f44e599c
Seth Mwebi <sethmwebi27@gmail.com>
2024-08-02 15:26:52 +0300

    writes code to pass props

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+const Links = ({ github, linkedin }) => {
+  return (
+    <>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </>
+  );
+};
+
+export default Links;
